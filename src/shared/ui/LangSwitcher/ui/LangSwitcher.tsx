065772151c720e1c0905570e9eb77451d0d4d1ee
Theseus <heavenlywork@outlook.com>
2023-02-11 15:24:11 +0300

    add i18n

---
+++ src/shared/ui/LangSwitcher/ui/LangSwitcher.tsx
@@ -0,0 +1,33 @@
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from './LangSwitcher.module.scss';
+import {useTranslation} from "react-i18next";
+import {Button} from "shared/ui/Button";
+import React from "react";
+import {ThemeButton} from "shared/ui/Button/ui/Button";
+
+interface LangSwitcherProps {
+    className?: string,
+
+}
+
+export const LangSwitcher = (props: LangSwitcherProps) => {
+    const {className} = props;
+
+    const {t, i18n} = useTranslation();
+    const onToggle = () => {
+        i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru')
+    }
+
+    return (
+        <Button
+            className={classNames(cls.LangSwitcher, {}, [className])}
+            onClick={onToggle}
+            theme={ThemeButton.CLEAR}
+        >
+            {t('Русский')}
+        </Button>
+    );
+};
+
+
+
