404ca2d0ac09a05258a470beb279a97e202e78e4
Theseus <heavenlywork@outlook.com>
2023-02-13 15:12:24 +0300

    add ErrorBoundary, PageError, BugButton, translations

---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,23 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { Button } from 'shared/ui/Button';
+import cls from './PageError.module.scss';
+
+interface PageErrorProps {
+    className?: string,
+
+}
+
+export const PageError = (props: PageErrorProps) => {
+    const { className } = props;
+    const { t } = useTranslation('pageError');
+    const reloadPage = () => {
+        window.location.reload();
+    };
+    return (
+        <div className={classNames(cls.PageError, {}, [className])}>
+            {t('Что-то пошло не так')}
+            <Button onClick={reloadPage}>{t('Перезагрузить страницу')}</Button>
+        </div>
+    );
+};
