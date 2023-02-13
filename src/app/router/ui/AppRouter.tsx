17aaceaa6438702602ac81ae81cad916c9733458
Theseus <heavenlywork@outlook.com>
2023-02-13 12:47:38 +0300

    add PageLoader, Loader, fix locales

--- src/app/router/ui/AppRouter.tsx
+++ src/app/router/ui/AppRouter.tsx
@@ -1,9 +1,10 @@
 import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'shared/ui/PageLoader';
 
 export const AppRouter = () => (
-    <Suspense fallback={<h2>Loading...</h2>}>
+    <Suspense fallback={<PageLoader />}>
         <Routes>
             {Object.values(routeConfig).map(({ element, path }) => (
                 <Route
