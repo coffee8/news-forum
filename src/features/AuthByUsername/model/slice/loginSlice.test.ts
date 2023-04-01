cac02f8ad8a17992d4ef5cd022f3ad51d23cef58
coffee8 <heavenlywork@outlook.com>
2023-04-01 12:45:40 +0300

    test loginSlice

---
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -0,0 +1,16 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { loginActions, loginReducer } from './loginSlice';
+import { LoginSchema } from '../types/loginSchema';
+
+describe('loginSlice.test', () => {
+    test('set username', () => {
+        const state: DeepPartial<LoginSchema> = { username: 'testname' };
+        expect(loginReducer(state as LoginSchema, loginActions.setUsername('testname')))
+            .toEqual({ username: 'testname' });
+    });
+    test('set password', () => {
+        const state: DeepPartial<LoginSchema> = { password: 'testpassword' };
+        expect(loginReducer(state as LoginSchema, loginActions.setPassword('testpassword')))
+            .toEqual({ password: 'testpassword' });
+    });
+});
