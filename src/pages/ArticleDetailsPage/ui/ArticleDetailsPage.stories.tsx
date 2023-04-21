aeb8fc2c33f8323f8d0f5d0dc1c48b6becd90015
coffee8 <heavenlywork@outlook.com>
2023-04-21 13:44:32 +0300

    add ArticlesPage, ArgicleDetailsPage, configure webpack

---
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import ArticleDetailsPage from './ArticleDetailsPage';
+
+export default {
+    title: 'shared/ArticleDetailsPage',
+    component: ArticleDetailsPage,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof ArticleDetailsPage>;
+
+const Template: ComponentStory<typeof ArticleDetailsPage> = (args) => <ArticleDetailsPage {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {};
---
+++ src/pages/ArticlesPage/ui/ArticlesPage.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import ArticlesPage from './ArticlesPage';
+
+export default {
+    title: 'shared/ArticlesPage',
+    component: ArticlesPage,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof ArticlesPage>;
+
+const Template: ComponentStory<typeof ArticlesPage> = (args) => <ArticlesPage {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {};
