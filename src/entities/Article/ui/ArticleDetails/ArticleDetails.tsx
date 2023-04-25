397d11d9e208672fb0cac16011038c170ee3799f
coffee8 <heavenlywork@outlook.com>
2023-04-25 19:18:45 +0300

    develop ArticleDetails, add Icon component, add assets

--- src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
+++ src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
@@ -4,8 +4,14 @@ import { DynamicModuleLoader, ReducersList } from 'shared/lib/DynamicModuleLoade
 import { useEffect } from 'react';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 import { useSelector } from 'react-redux';
-import { Text, TextAlign, TextTheme } from 'shared/ui/Text/Text';
+import {
+    Text, TextAlign, TextSize, TextTheme,
+} from 'shared/ui/Text/Text';
 import { Skeleton } from 'shared/ui/Skeleton/Skeleton';
+import { Avatar } from 'shared/ui/Avatar/Avatar';
+import { Icon } from 'shared/ui/Icon/Icon';
+import CalendarIcon from 'shared/assets/icons/calendar-20-20.png';
+import EyeIcon from 'shared/assets/icons/eye-20-20.png';
 import { fetchArticleById } from '../../model/services/fetchArticleById/fetchArticleById';
 import {
     getArticleDetailsData,
@@ -28,9 +34,8 @@ export const ArticleDetails = (props: ArticleDetailsProps) => {
     const { t } = useTranslation();
     const dispatch = useAppDispatch();
     const error = useSelector(getArticleDetailsError);
-    // const isLoading = useSelector(getArticleDetailsIsLoaiding);
-    const isLoading = true;
-    const data = useSelector(getArticleDetailsData);
+    const isLoading = useSelector(getArticleDetailsIsLoaiding);
+    const article = useSelector(getArticleDetailsData);
 
     useEffect(() => {
         dispatch(fetchArticleById(id));
@@ -40,13 +45,13 @@ export const ArticleDetails = (props: ArticleDetailsProps) => {
 
     if (isLoading) {
         content = (
-            <div>
+            <>
                 <Skeleton className={cls.avatar} width={200} height={200} border="50%" />
                 <Skeleton className={cls.title} width={300} height={32} />
                 <Skeleton className={cls.skeleton} width={600} height={24} />
                 <Skeleton className={cls.skeleton} width="100%" height={200} />
                 <Skeleton className={cls.skeleton} width="100%" height={200} />
-            </div>
+            </>
         );
     } else if (error) {
         content = (
@@ -58,7 +63,30 @@ export const ArticleDetails = (props: ArticleDetailsProps) => {
         );
     } else {
         content = (
-            t('ArticleDetails')
+            <>
+                <div className={cls.avatar}>
+                    <Avatar
+                        className={cls.avatar}
+                        src={article?.img}
+                        size={200}
+                        alt={t('Изображение статьи')}
+                    />
+                </div>
+                <Text
+                    className={cls.title}
+                    title={article?.title}
+                    text={article?.subtitle}
+                    size={TextSize.L}
+                />
+                <div className={cls.articleInfo}>
+                    <Icon className={cls.icon} Svg={EyeIcon} />
+                    <Text text={String(article?.views)} />
+                </div>
+                <div className={cls.articleInfo}>
+                    <Icon className={cls.icon} Svg={CalendarIcon} />
+                    <Text text={String(article?.createdAt)} />
+                </div>
+            </>
         );
     }
     return (
