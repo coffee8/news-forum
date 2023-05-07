a6716474f985e9a04bbd60c9dd66b8d3baefd0a8
coffee8 <heavenlywork@outlook.com>
2023-05-07 14:26:32 +0300

    develop comments section

--- src/pages/ArticleDetailsPage/model/slices/articleDetailsCommentsSlice.ts
+++ src/pages/ArticleDetailsPage/model/slices/articleDetailsCommentsSlice.ts
@@ -1,9 +1,10 @@
 import {
     createEntityAdapter,
-    createSlice,
+    createSlice, PayloadAction,
 } from '@reduxjs/toolkit';
 import { Comment } from 'entities/Comment';
 import { StateSchema } from 'app/providers/StoreProvider';
+import { fetchCommentsByArticleId } from 'pages/ArticleDetailsPage/model/services/fetchCommentsByArticleId';
 import { ArticleDetailsCommentsSchema } from '../types/ArticleDetailsCommentsSchema';
 
 const commentsAdapter = createEntityAdapter<Comment>({
@@ -19,21 +20,25 @@ const articleDetailsCommentsSlice = createSlice({
     initialState: commentsAdapter.getInitialState<ArticleDetailsCommentsSchema>({
         isLoading: false,
         error: undefined,
-        ids: ['1', '2'],
-        entities: {
-            1: {
-                id: '1',
-                user: { id: '1', username: 'User 1' },
-                body: 'text 1',
-            },
-            2: {
-                id: '2',
-                user: { id: '2', username: 'User 2' },
-                body: 'text 2',
-            },
-        },
+        ids: [],
+        entities: {},
     }),
     reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchCommentsByArticleId.pending, (state) => {
+                state.error = undefined;
+                state.isLoading = true;
+            })
+            .addCase(fetchCommentsByArticleId.fulfilled, (state, action: PayloadAction<Comment[]>) => {
+                state.isLoading = false;
+                commentsAdapter.setAll(state, action.payload);
+            })
+            .addCase(fetchCommentsByArticleId.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
+            });
+    },
 });
 
 export const { reducer: articleDetailsCommentsReducer } = articleDetailsCommentsSlice;
--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage.tsx
@@ -6,7 +6,10 @@ import { ArticleDetails } from 'entities/Article';
 import { Text } from 'shared/ui/Text/Text';
 import { CommentList } from 'entities/Comment';
 import { DynamicModuleLoader, ReducersList } from 'shared/lib/DynamicModuleLoader/DynamicModuleLoader';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { getArticleCommentsIsLoading } from 'pages/ArticleDetailsPage/model/selectors/comments';
+import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
+import { fetchCommentsByArticleId } from '../model/services/fetchCommentsByArticleId';
 import { articleDetailsCommentsReducer, getArticleComments } from '../model/slices/articleDetailsCommentsSlice';
 import cls from './ArticleDetailsPage.module.scss';
 
@@ -22,20 +25,27 @@ const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
     const { className } = props;
     const { t } = useTranslation('article');
     const { id } = useParams<{ id: string }>();
+    const dispatch = useDispatch();
     const comments = useSelector(getArticleComments.selectAll);
+    const commentsIsLoading = useSelector(getArticleCommentsIsLoading);
+
+    useInitialEffect(() => {
+        dispatch(fetchCommentsByArticleId(id));
+    });
 
     if (!id) {
         return (
             <div className={classNames(cls.ArticleDetailsPage, {}, [className])} />
         );
     }
+
     return (
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
             <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
                 <ArticleDetails id={id} />
                 <Text className={cls.commentTitle} title={t('Комментарии')} />
                 <CommentList
-                    isLoading
+                    isLoading={commentsIsLoading}
                     comments={comments}
                 />
             </div>
