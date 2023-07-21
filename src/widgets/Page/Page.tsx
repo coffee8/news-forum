06fb4ac039261421d3c580959f8fdac7fccaa22a
coffee8 <heavenlywork@outlook.com>
2023-07-21 20:27:53 +0500

    fix scroll

--- src/widgets/Page/Page.tsx
+++ src/widgets/Page/Page.tsx
@@ -46,7 +46,7 @@ export const Page = (props: PageProps) => {
             onScroll={onScroll}
         >
             {children}
-            <div ref={triggerRef} />
+            { onScrollEnd && <div className={cls.trigger} ref={triggerRef} /> }
         </section>
     );
 };
