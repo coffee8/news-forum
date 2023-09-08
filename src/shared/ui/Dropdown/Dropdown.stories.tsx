3a5710eaf0c66220e2ce2ac6201fbd0f48a152b9
coffee8 <heavenlywork@outlook.com>
2023-09-08 15:26:46 +0300

    fix: imports, css

--- src/shared/ui/Dropdown/Dropdown.stories.tsx
+++ src/shared/ui/Dropdown/Dropdown.stories.tsx
@@ -3,7 +3,7 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
-import { Button } from 'shared/ui/Button/Button';
+import { Button } from '../Button/Button';
 import { Dropdown } from './Dropdown';
 
 export default {
